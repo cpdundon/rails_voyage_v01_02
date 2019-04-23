65c6a30ef6b7ee4f5e963ffba3a6d47df4876a1e
Christopher P. Dundon <chris.dundon@gmail.com>
2019-04-22 23:06:16 -0400

    got js-jquery up and running.  built out straw structure for the show page

---
+++ app/assets/javascripts/vessle_show.js
@@ -0,0 +1,6 @@
+//Code for the Vessel Show Page
+$(document).ready(function () {
+//	let game = new Game(-1);
+//	game.attachListeners();
+	$('#vessel_name')[0].innerText = "Test Test";
+});
---
+++ app/serializers/vessel_serializer.rb
@@ -0,0 +1,3 @@
+class VesselSerializer < ActiveModel::Serializer
+  attributes :id, :name, :active
+end
