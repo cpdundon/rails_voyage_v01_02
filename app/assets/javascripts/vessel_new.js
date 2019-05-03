58385533fb4d7320e090bf7a51ed7815b326d913
Christopher P. Dundon <chris.dundon@gmail.com>
2019-05-02 20:29:12 -0400

    starting code clean-up.  need to commit state before modifying everything

--- app/assets/javascripts/vessel_new.js
+++ app/assets/javascripts/vessel_new.js
@@ -5,6 +5,18 @@ class VesselNew {
 	}
 
 	submit (url) {
+		$.post("/vessels", this.data)
+			.done( data => {
+				const newID = data.id;
+				const vesselShow = new VesselShow();
+				const url = window.location.origin + '/vessels/' + newID;
+				vesselShow.populate(url);
+				debugger;
+		});
+
+return false;
+debugger;
+
 		$.ajax({
       type: 'POST',
       url: url,
